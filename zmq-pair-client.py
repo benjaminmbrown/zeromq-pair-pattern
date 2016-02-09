e33d16803d69516700c9b02a3137ce553a5e2a39
Benjamin M. Brown <benjaminbb@gmail.com>
2016-02-08 19:55:13 -0600

    building out pair pattern client code

--- zmq-pair-client.py
+++ zmq-pair-client.py
@@ -1,9 +1,21 @@
 #initialize any zmq libs
 import zmq
 import time
+import sys
+import time
+
+port = "5556"
+
 #multiple contexts can be created in an app.
 #contexts are thread-safe unlike sockets
 context = zmq.Context()
 
 #create socket from the context
 socket = context.socket(zmq.REP)
+socket.bind("tcp://*:%s" % port)
+
+while True:
+	socket.send("Server message to client3")
+	msg= socket.recv()
+	print msg
+	time.sleep(1)
